a63a2b662fb0651cd7dd73efa421010ba8afcaa2
co2p <go.co2p@gmail.com>
2014-10-07 18:00:40 +0200

    Init

--- src/eson/co2p/se/Main.java
+++ src/eson/co2p/se/Main.java
@@ -1,8 +1,25 @@
 package eson.co2p.se;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Thread nameServer;
+        ArrayList<Thread> threads = new ArrayList<Thread>();
+
+
+
+        nameServer = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+        threads.add(nameServer);
+        nameServer.start();
+
+
+
     }
 }
