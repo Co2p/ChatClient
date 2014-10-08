4fb185f3d5b050ca3eaba961b43f5877c53ffe7e
co2p <go.co2p@gmail.com>
2014-10-08 10:08:57 +0200

    Init

---
+++ src/eson/co2p/se/nameServerPing.java
@@ -0,0 +1,14 @@
+package eson.co2p.se;
+
+import com.sun.net.httpserver.HttpServer;
+
+/**
+ * Created by gordon on 07/10/14.
+ *
+ * Pings the name server
+ */
+public class nameServerPing {
+
+
+
+}
