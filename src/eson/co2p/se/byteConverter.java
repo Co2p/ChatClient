e860ebb1728575f3f706fff45be0bb219678e0e8
isidornygren <karlisidornygren@gmail.com>
2014-10-08 14:08:47 +0200

    scoobydooby ey

--- src/eson/co2p/se/byteConverter.java
+++ src/eson/co2p/se/byteConverter.java
@@ -2,6 +2,7 @@ package eson.co2p.se;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.io.*;
 
 /**
  * Created by gordon on 08/10/14.
@@ -10,16 +11,32 @@ import java.util.ArrayList;
  */
 public class byteConverter {
 
-    Byte zeroByte;
-
     public void headerBuilder(ArrayList<Integer> format, ArrayList<String> content){
         // Go trough the format-Array which contains the length of each element in the
         // array "content" The string is expected to be in UTF-8.
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream( );
+
         for(int i = 0;i < format.size(); i++){
             //TODO  skapar en enkel sträng här bara för test, denna ska egentligen vara
             //      en ström av bytes
             String test;
-
+            //skapar en ny byte med bytelängden som finns i formatArrayen
+            byte[] stringByte = new byte[format.get(i)];
+            //Lägger in infot som finns i contentArrayen
+            try {
+                stringByte = content.get(i).getBytes("UTF-8");
+                outputStream.write(stringByte);
+            }catch(Exception e){
+                System.out.println("Här gick det lite fel");
+            }
         }
+        byte totBytes[] = outputStream.toByteArray();
+        //testskrivut bytesen
+        System.out.println("Printing bytes:");
+        for(int i = 0; i < totBytes.length; i++){
+            int print = totBytes[i] & 0xff;
+            System.out.print(print);
+        }
+        System.out.println("Printing done!");
     }
 }
