389d32d076119a8eee3f91da66f13a3b895158d4
co2p <go.co2p@gmail.com>
2014-10-08 22:07:46 +0200

    weird thread

--- src/eson/co2p/se/clientThread.java
+++ src/eson/co2p/se/clientThread.java
@@ -1,5 +1,7 @@
 package eson.co2p.se;
 
+import java.util.ArrayList;
+
 /**
  * Created by Tony on 07/10/2014.
  *
@@ -8,9 +10,23 @@ package eson.co2p.se;
 public class clientThread {
 
     private startGui gui;
+    private Thread senderThread;
+    private senderServer sender;
+    private ArrayList<Thread> threads = new ArrayList<Thread>();
 
     //skapa konstruktor
-    public clientThread(startGui gui){
+    public clientThread(startGui gui) {
+
+        private void startSender () {
+            senderThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    sender = new senderServer();
+                }
+            });
+            threads.add(senderThread);
+            senderThread.start();
+        }
 
     }
 }
---
+++ src/eson/co2p/se/senderServer.java
@@ -0,0 +1,7 @@
+package eson.co2p.se;
+
+/**
+ * Created by gordon on 08/10/14.
+ */
+public class senderServer {
+}
