383812996944614f1ac43cf3318fb6b9b6211191
co2p <go.co2p@gmail.com>
2014-10-08 11:01:36 +0200

    Init

--- src/eson/co2p/se/byteConverter.java
+++ src/eson/co2p/se/byteConverter.java
@@ -1,10 +1,16 @@
 package eson.co2p.se;
 
+import java.lang.reflect.Array;
+
 /**
  * Created by gordon on 08/10/14.
  *
  * Handles the conversion to bytestreams, and adds padding
  */
 public class byteConverter {
+
+    Byte zeroByte;
+
+    public void headerBuilder()
 
 }
--- src/eson/co2p/se/clientThread.java
+++ src/eson/co2p/se/clientThread.java
@@ -4,6 +4,7 @@ package eson.co2p.se;
  * Created by Tony on 07/10/2014.
  */
 public class clientThread {
+
     //skapa konstruktor
     public clientThread(int port, startGui gui){
 
