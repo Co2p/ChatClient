216f468009e75d46c760b7a76717a7b8a2baee40
isidornygren <karlisidornygren@gmail.com>
2014-10-09 15:19:02 +0200

    scoobydooby ey

--- src/eson/co2p/se/testNameServer.java
+++ src/eson/co2p/se/testNameServer.java
@@ -7,6 +7,8 @@ import java.net.*;
 /**
  * Created by root on 2014-10-09.
  */
+
+// http://systembash.com/content/a-simple-java-udp-server-and-udp-client/
 public class testNameServer {
     String nameServer = "itchy.cs.umu.se";
     int nameServerPort = 1337;
