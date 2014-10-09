7e263a3d618797d7b411a2b7bfdc16da8b9f98bc
isidornygren <karlisidornygren@gmail.com>
2014-10-09 22:40:09 +0200

    scoobydooby ey

--- src/eson/co2p/se/byteConverter.java
+++ src/eson/co2p/se/byteConverter.java
@@ -28,8 +28,6 @@ public class byteConverter {
             totForm += format.get(i);
         }
         byte[] sendBytes = new byte[totForm];
-
-        //Här kommer lite ny kod
         for(int i = 0; i < totForm; i++){
             if(content.get(i) instanceof Integer){
                 int intConv = (Integer)content.get(i);
@@ -42,35 +40,7 @@ public class byteConverter {
                 }
             }
             if(format.get(i) > 1){i += (format.get(i) - 1);}
-            //Här ska även checkar om den skrivit ut alla nollor, detta har inte gjorts än.
         }
-
         return sendBytes;
-
-        /*//här är gamla koden
-        byte OP = (byte) OPCode;
-        stringByte[0] = OP;
-        for(int i = 1;i < format.size(); i++){
-            try {
-                if(content.get(i) != null) {
-                    //stringByte[i] = content.get(i).getBytes("UTF-8");;
-                    //If the added content is smaller than the given bytelength, add /0's until everything is filled
-                    //for(int j = content.get(i).getBytes("UTF-8").length;
-                    //    j < format.get(i); j++){
-                    //    outputStream.write("\0".getBytes("UTF-8"));
-                    //}
-                }else{
-                    for(int j = 0; j < format.get(i); j++){
-                        stringByte[i] = (byte)0;
-                        //outputStream.write("\0".getBytes("UTF-8"));
-                    }
-                }
-
-            }catch(Exception e){
-                System.out.println("Exception occurred during byte-conversion: " + e);
-                e.printStackTrace();
-            }
-        }
-        return outputStream;*/
     }
 }
