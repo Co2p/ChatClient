2e8ce4c2d22ae4c3aeb906edb4e37307493b148c
co2p <go.co2p@gmail.com>
2014-10-09 18:38:53 +0200

    senderServer tar emot saker

--- src/eson/co2p/se/senderServer.java
+++ src/eson/co2p/se/senderServer.java
@@ -1,9 +1,56 @@
 package eson.co2p.se;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by gordon on 08/10/14.
  *
  * TCP connection to the Chat Server
  */
 public class senderServer {
+
+    private ServerSocket localServerSocket;
+    private BufferedReader byteArrayInput;
+    private char[] charBuff = new char[65535];
+
+    public senderServer() {
+
+        try {
+            localServerSocket = new ServerSocket(1339);
+
+
+            try {
+                Socket localSocket = null;
+                localSocket = localServerSocket.accept();
+
+
+                try {
+                    byteArrayInput = new BufferedReader(new InputStreamReader(localSocket.getInputStream()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String listen(){
+
+        try {
+            byteArrayInput.read(charBuff);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(charBuff);
+
+        return charBuff.toString();
+    }
 }
