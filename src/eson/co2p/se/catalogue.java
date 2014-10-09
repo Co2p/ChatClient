200dbd92814e04ce3a7ed8985133449a7650129b
co2p <go.co2p@gmail.com>
2014-10-09 13:32:26 +0200

    comments

--- src/eson/co2p/se/catalogue.java
+++ src/eson/co2p/se/catalogue.java
@@ -11,34 +11,66 @@ public class catalogue {
     private String serverIP;
     private String serverPort;
 
+    /**
+     * Sets the ID number that the client received from the server
+     * @param idNumber the ID number
+     */
     public void setIdNumber(int idNumber) {
         this.idNumber = idNumber;
     }
 
+    /**
+     * Sets the user name
+     * @param name user name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Saves the server IP address
+     * @param serverIP the IP as a String
+     */
     public void setServerIP(String serverIP) {
         this.serverIP = serverIP;
     }
 
+    /**
+     * Saves the server port number
+     * @param serverPort portnumber as a String
+     */
     public void setServerPort(String serverPort) {
         this.serverPort = serverPort;
     }
 
+    /**
+     * Returns the client ID number
+     * @return ID number
+     */
     public int getIdNumber() {
         return idNumber;
     }
 
+    /**
+     * Returns the user name
+     * @return user name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns the server IP address
+     * @return server IP
+     */
     public String getServerIP() {
         return serverIP;
     }
 
+    /**
+     * Returns the server port
+     * @return server port
+     */
     public String getServerPort() {
         return serverPort;
     }
