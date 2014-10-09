937d37612fc72772988c7d7028f8ace7fc2dd42b
isidornygren <karlisidornygren@gmail.com>
2014-10-09 15:18:30 +0200

    scoobydooby ey

---
+++ src/eson/co2p/se/testNameServer.java
@@ -0,0 +1,21 @@
+package eson.co2p.se;
+
+import java.io.*;
+import java.net.*;
+
+
+/**
+ * Created by root on 2014-10-09.
+ */
+public class testNameServer {
+    String nameServer = "itchy.cs.umu.se";
+    int nameServerPort = 1337;
+    String nick = "Shit";
+
+    public static void main(String[] args){
+        udpGetServers();
+    }
+    private static void udpGetServers(){
+
+    }
+}
