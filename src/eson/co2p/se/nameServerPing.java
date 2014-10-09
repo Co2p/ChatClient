44dab873ba36f8294a7aba742a764586e3c0144c
co2p <go.co2p@gmail.com>
2014-10-09 15:34:19 +0200

    a method to ping the server.. and some comments

--- src/eson/co2p/se/nameServerPing.java
+++ src/eson/co2p/se/nameServerPing.java
@@ -2,10 +2,53 @@ package eson.co2p.se;
 
 import com.sun.net.httpserver.HttpServer;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+
 /**
  * Created by gordon on 07/10/14.
  *
  */
 public class nameServerPing {
 
+    private String ip, port;
+    private int localPort = 1337;
+    private ServerSocket serverSocket;
+    private ArrayList<Integer> format;
+    private ArrayList<String> content;
+
+    /**
+     * Constructor
+     * @param ip Server IP
+     * @param port Server Port
+     */
+    public nameServerPing(String ip, String port){
+        this.ip=ip;
+        this.port=port;
+
+        serverPing();
+    }
+
+    /**
+     * Sends a UDP request to the nameserver
+     */
+    public void serverPing(){
+        try {
+            serverSocket = new ServerSocket(localPort);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        DataOutputStream out;
+        try {
+            out = new DataOutputStream(serverSocket.accept().getOutputStream());
+            out.write(byteConverter.headerBuilder(format, content).toByteArray());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
