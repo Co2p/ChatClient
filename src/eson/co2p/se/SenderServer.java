e06cc935bd8c5fe673cecf449b4b6712ff5f5d2c
isidornygren <karlisidornygren@gmail.com>
2014-10-18 22:14:02 +0200

    scoobydooby ey

--- src/eson/co2p/se/SenderServer.java
+++ src/eson/co2p/se/SenderServer.java
@@ -2,8 +2,6 @@ package eson.co2p.se;
 
 import java.io.*;
 import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
@@ -16,8 +14,9 @@ public class SenderServer{
     InetAddress Ip;
     private Socket localServerSocket;
     private PrintStream outToServer;
-    private BufferedReader inFromServer;
-    String recivedData;
+    //private BufferedReader inFromServer;
+    private DataInputStream in;
+    String receivedData;
     startGui GUI;
     int Tabid;
 
@@ -29,25 +28,26 @@ public class SenderServer{
         ConnectSocket();
         System.out.println("getting working GUI");
         GUI = catalogue.getGui();
-        System.out.println("Starting message chek!");
-        checkReciveMessage();
+        System.out.println("Starting message check!");
+        checkReceivedMessage();
 
     }
 
     //connect the socket to given ip/port
     private void ConnectSocket(){
-        int Tryes = 0;
-        int MaxTryes = 5;
-        while(Tryes < MaxTryes) {
+        int tries = 0;
+        int maxTries = 5;
+        while(tries < maxTries) {
             try {
                 localServerSocket = new Socket(Ip, Port);
                 localServerSocket.setSoTimeout(500);
-                inFromServer = new BufferedReader(new InputStreamReader(localServerSocket.getInputStream()));
+                in = new DataInputStream(localServerSocket.getInputStream());
+                //inFromServer = new BufferedReader(new InputStreamReader(localServerSocket.getInputStream()));
                 System.out.println("connected socket!");
-                Tryes = MaxTryes;
+                tries = maxTries;
             } catch (IOException e) {
-                Tryes += 1;
-                System.out.println("Failed to bind socket, try " + Tryes + "/" + MaxTryes);
+                tries += 1;
+                System.out.println("Failed to bind socket, try " + tries + "/" + maxTries);
             }
         }
 
@@ -70,7 +70,7 @@ public class SenderServer{
         }
     }
     //chek if this thread is still alive
-    public boolean endSocketChek(){
+    public boolean endSocketCheck(){
         int myLife = ClientThread.AliveThreadsID[Tabid];
         if (myLife == 1){
             return true;
@@ -84,26 +84,73 @@ public class SenderServer{
     private String GetMessageToSend(){
         String Message;
         while(true){
-            if(catalogue.MessageInUse == false){
-                //System.out.println("getting message to send message");
+            if(!catalogue.MessageInUse){
                 Message = catalogue.GetClientMessage(Tabid);
-                System.out.println("wadaw:" + Message);
-                //System.out.println("Loop 2 broken, message:" + Message);
-                if (Message == null){
-                    return null;
+                System.out.println("The message (GetMessageToSend): " + Message);
+                if (Message != null){
+                    System.out.println("Message broken, sorry bro...");
                 }
-                else{
-                    System.out.println("Loop 2 broken, message:" + Message);
-                    return Message;
-                }
+                break;
             }
         }
+        return Message;
     }
-    //chek for recived messages on this socket
-    private void checkReciveMessage(){
-        while(endSocketChek()) {
-            boolean Goon = false;
-            //System.out.println("where here");
+    //check for received messages on this socket
+    private void checkReceivedMessage(){
+        boolean goOn = true;
+        boolean firstRun = true;
+        int totBytesRead = 0;
+        String messageString = "";
+        byte[] messageByte = new byte[1000];
+        int totLength = 0;
+        PDU message = new PDU(0);
+
+        while(endSocketCheck() && goOn) {
+            System.out.println("In while-loop");
+            try {
+                int bytesRead = in.read(messageByte);
+                totBytesRead += bytesRead;
+                if(bytesRead > 8) {
+                    //TODO antar bara här att det är ett meddelande som tas emot, ISIDOR KOM IHÅG FÖR FAN DIN JÄVLA BÖG
+                    PDU temp = new PDU(messageByte, messageByte.length);
+                    System.out.println("OP-code: " + (int)temp.getByte(0));
+                    if(firstRun && temp.getByte(0) == OpCodes.MESSAGE) { //kollar op
+                        firstRun = false;
+                        System.out.println("Found message!");
+                        totLength = 12;
+                        totLength += temp.getByte(2);
+                        totLength += temp.getByte(4);
+                        totLength = Message.div4(totLength);
+                        message.extendTo(totLength);
+                    }else if(firstRun && temp.getByte(0) == OpCodes.NICKS){
+                        firstRun = false;
+                        System.out.println("Found nicks!");
+                        totLength = 4;
+                        totLength += Message.div4(temp.getShort(2));
+                        message.extendTo(totLength);
+                    }
+                    //messageString += new String(messageByte, 0, bytesRead);
+                    System.out.println("totLength = " + totLength + ", bytesRead = " + bytesRead +
+                            ", totBytesRead = " + totBytesRead + ", length of byte-array = " + messageByte.length);
+                    message.setSubrange((totBytesRead - bytesRead), temp.getSubrange(0,bytesRead));
+                    if (totBytesRead >= totLength) {
+                        goOn = false;
+                        String nicknames = new String(message.getSubrange(4,totLength - 5), "UTF-8");
+                        System.out.println("Connected users: " + nicknames);
+                    }
+                }else{
+                    System.out.println("TCP acting wierd, input not large enough etc.");
+                    System.exit(0);
+                }
+            }catch(IOException e){
+                //Read time out, this should happen once every one second.
+                //System.out.println(e);
+            }
+
+
+            /*goOn = false;
+
+            System.out.println("checkReceivedMessage start");
             String Mess = GetMessageToSend();
             if(Mess != null){
                 sendMessage(Mess,0);
@@ -111,21 +158,22 @@ public class SenderServer{
             }
             try {
                 try {
-                    recivedData = inFromServer.readLine();
-                    String Message = recivedData;
-                    while(recivedData != null) {
-                        recivedData = inFromServer.readLine();
-                        Message = Message + recivedData;
+                    receivedData = inFromServer.readLine();
+                    String Message = receivedData;
+                    System.out.println("Received message: " + Message);
+                    while(receivedData != null) {
+                        receivedData = inFromServer.readLine();
+                        Message = Message + receivedData;
                     }
                     if (Message != null){
-                        Goon = true;
+                        goOn = true;
                     }
                 }catch (java.net.SocketTimeoutException e){
-                    System.out.println("Timed out trying to read from socket(this is supose to happend once every sec)");
+                    System.out.println("Socket timeout (Don't worry)");
                 }
-                if (Goon == true) {
-                    System.out.println("Updating gui WHIT MESSAGE: " + recivedData);
-                    GUI.UpdateTabByID(Tabid, recivedData);
+                if (goOn == true) {
+                    System.out.println("Updating gui WITH MESSAGE: " + receivedData);
+                    GUI.UpdateTabByID(Tabid, receivedData);
                     try {
                         Thread.sleep(10);
                     } catch (InterruptedException e) {
@@ -134,14 +182,16 @@ public class SenderServer{
                 }
             } catch (IOException e) {
                 System.out.println("Failed to get message");
-            }
+            }*/
         }
+        System.out.println("MESSAGE: " + messageString);
+        /*
         try {
             outToServer.write(Message.quitServer());
             localServerSocket.close();
             System.out.println("Closing this instance of SenderServer");
         }catch (IOException e){
             System.out.println("Failed to close socket");
-        }
+        }*/
     }
 }
