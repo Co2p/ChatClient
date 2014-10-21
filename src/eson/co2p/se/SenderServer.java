7dbf62772c94aca3b1e5d7717323fa86d1c69917
isidornygren <karlisidornygren@gmail.com>
2014-10-21 03:02:42 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/eson/co2p/se/catalogue.java

--- src/eson/co2p/se/SenderServer.java
+++ src/eson/co2p/se/SenderServer.java
@@ -1,5 +1,4 @@
 package eson.co2p.se;
-import java.awt.*;
 import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
@@ -86,12 +85,7 @@ public class SenderServer{
      */
     public boolean endSocketCheck(){
         int myLife = ClientThread.AliveThreadsID[Tabid];
-        if (myLife == 1){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return myLife == 1;
     }
     //TODO comment?
     private String GetMessageToSend(){
@@ -99,9 +93,6 @@ public class SenderServer{
         while(true){
             if(!catalogue.MessageInUse){
                 Message = catalogue.GetClientMessage(Tabid);
-                if (Message != null){
-                    //System.out.println("Message broken, sorry bro...no message is not broken, yes it is!");
-                }
                 break;
             }
         }
@@ -130,13 +121,9 @@ public class SenderServer{
         }
     }
     private void checkReceivedMessage(){
-        boolean goOn = true;
-        boolean firstRun = true;
-        int totBytesRead, totLength;
-        String messageString = "";
         byte[] messageByte = new byte[1000];
         PDU message = new PDU(0);
-        while(endSocketCheck() && goOn) {
+        while(endSocketCheck()) {
             String Messagelol = GetMessageToSend();
             if (Messagelol != null){
                 //Check if the message contains a command
@@ -160,6 +147,7 @@ public class SenderServer{
                     }
                 }
             }catch(IOException e){
+                //don't put anything here as it will cramp the terminal
             }
         }
         //After endsocketcheck, disconnect from the server
