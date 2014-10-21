68c34faabfa2c71487261e17d0618688d95191f3
isidornygren <karlisidornygren@gmail.com>
2014-10-22 00:38:34 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/eson/co2p/se/catalogue.java

--- src/eson/co2p/se/startGui.java
+++ src/eson/co2p/se/startGui.java
@@ -109,12 +109,12 @@ public class startGui extends JFrame implements ActionListener {
                     StyleConstants.setBackground(keyWord, new Color(255, 169, 170));
                     break;
             }
-            StyleConstants.setForeground(keyWord, Color.DARK_GRAY);
-            doc.insertString(doc.getLength(), "\n[kl." + time + "] ", keyWord);
+            StyleConstants.setForeground(keyWord, Color.LIGHT_GRAY);
+            doc.insertString(doc.getLength(), "\n[kl. " + time + "] ", keyWord);
             if (userName.length() > 0) {
                 StyleConstants.setBold(keyWord, true);
                 StyleConstants.setForeground(keyWord, colorFromString(userName));
-                doc.insertString(doc.getLength(), userName + " ", keyWord);
+                doc.insertString(doc.getLength(), userName + ": ", keyWord);
                 StyleConstants.setBold(keyWord, false);
 
             }
