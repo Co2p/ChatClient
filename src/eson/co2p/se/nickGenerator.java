2f96dcaab39a4e39558bdb382f7a92c2d6ba77db
co2p <go.co2p@gmail.com>
2014-10-22 11:25:17 +0200

    cleanup

--- src/eson/co2p/se/nickGenerator.java
+++ src/eson/co2p/se/nickGenerator.java
@@ -10,13 +10,14 @@ import java.util.Random;
 public class nickGenerator {
     private static String[] colorNicks = new String[] {"Red", "Blue", "Green", "Purple", "Orange", "Turquoise", "Orange",
             "Cyan", "Pink", "Brown", "Black", "White", "Hot", "Cold", "Sour", "Sweet", "Slippery", "Big", "Small",
-            "Enormous", "Fine", "Pulsating", "Hacking", "Freaky", "Epic", "Awesome", "Super",
-            "Flash", "Growing", "Changing", "Sonic", "Drippery", "Fucking", "Ganja", "420", "Horny", "Slobbering"};
+            "Enormous", "Fine", "Pulsating", "Hacking", "Freaky", "Epic", "Awesome", "Super", "Flash", "Growing",
+            "Changing", "Sonic", "Holy", "Drippery", "Fucking", "Ganja", "420", "Horny", "Slobbering"};
 
     private static String[] nicks = new String[] {"Panther", "Carrot", "Cactus", "Sea", "Tiger", "Cat", "Dog", "Warthog",
             "Leopard", "Flower", "Circuit", "Sun", "Star", "Galaxy", "Kangaroo", "Pig", "Cow", "Frog", "Toad",
             "Possum", "Corpse", "Hat", "Punch", "Calculus", "Signal", "Pond", "Brother", "Sister", "Bandit", "Sanic",
             "Dick", "Anal", "Penis", "Ass", "Kånkelbär", "Dildo", "Cock", "Weed", "Ganja", "Aina", "Knatch"};
+
     private static int[] offset = new int[] {0, 0};
 
     /**
